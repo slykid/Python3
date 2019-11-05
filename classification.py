80ecbeeb57e1a525c967e065e82c0dc156ef6b4f
slykid <slykid@naver.com>
2019-11-05 22:25:21 +0900

    Python Machine Learning : Ensenble(MajorityVoteClassifier)

--- classification.py
+++ classification.py
@@ -572,3 +572,157 @@ y_pred = bag_clf.predict(x_test)
 accuracy_score(y_test, y_pred) #  1.0
 
 bag_clf.oob_decision_function_
+
+# 다수결투표를 사용한 분류기
+import numpy as np
+np.argmax(np.bincount([0, 0, 1],
+                      weights=[0.2, 0.2, 0.6]))
+
+
+## 다수결투표분류기
+### 클래스 제작을 위한 라이브러리
+from sklearn.base import BaseEstimator
+from sklearn.base import ClassifierMixin
+from sklearn.base import clone
+from sklearn.preprocessing import LabelEncoder
+from sklearn.externals import six
+from sklearn.pipeline import _name_estimators
+
+### 훈련을 위한 라이브러리
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+### 모델 생성(분류기 모델만 사용)
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+
+### ROC Curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+
+import numpy as np
+import operator
+from matplotlib import pyplot as plt
+
+class MajorityVoteClassifier (BaseEstimator, ClassifierMixin):
+    def __init__(self, classifiers, vote="classlabel", weights=None):
+        self.classifiers = classifiers
+        self.named_classifiers = {key : value for key, value in _name_estimators(classifiers)}
+        self.vote = vote
+        self.weights = weights
+
+    def fit(self, x, y):
+        self.lablenc_ = LabelEncoder()
+        self.lablenc_.fit(y)
+        self.classes_ = self.lablenc_.classes_
+        self.classifiers_ = []
+
+        for clf in self.classifiers:
+            fitted_clf = clone(clf).fit(x, self.lablenc_.transform(y))
+            self.classifiers_.append(fitted_clf)
+
+        return self
+
+    def predict(self, x):
+        if self.vote == 'probability':
+            maj_vote = np.argmax(self.predict_proba(x), axis=1)
+        else:
+            predictions = np.asarray([clf.predict(x) for clf in self.classifiers_])
+            maj_vote = np.apply_along_axis(lambda x : np.argmax(np.bincount(x, weights=self.weights)),
+                                           axis=1,
+                                           arr=predictions)
+
+        maj_vote = self.lablenc_.inverse_transform(maj_vote)
+
+        return maj_vote
+
+    def predict_proba(self, x):
+        probas = np.asarray([clf.predict_proba(x) for clf in self.classifiers_])
+        avg_proba = np.average(probas, axis=0, weights=self.weights)
+
+        return avg_proba
+
+    def get_params(self, deep=True):
+        if not deep:
+            return super(MajorityVoteClassifier, self).get_params(deep=False)
+        else:
+            out = self.named_classifiers.copy()
+            for name, step in six.iteritems(self.named_classifiers):
+                for key, value in six.iteritems(step.get_params(deep=True)):
+                    out['%s__%s' % (name, key)] = value
+
+        return out
+
+iris = datasets.load_iris()
+
+x, y = iris.data[50:, [1,2]], iris.target[50:]
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.5, random_state=1, stratify=y)
+
+clf1 = LogisticRegression(solver="liblinear",
+                          penalty="l2",
+                          C=0.001,
+                          random_state=1)
+clf2 = DecisionTreeClassifier(max_depth=1,
+                              criterion="entropy",
+                              random_state=0)
+clf3 = KNeighborsClassifier(n_neighbors=1,
+                            p=2,
+                            metric="minkowski")
+
+pipe1 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf1]])
+pipe3 = Pipeline([['sc', StandardScaler()],
+                  ['clf', clf3]])
+clf_labels = ['Logistic Regression', 'Decision Tree', 'KNN']
+
+### 10-Fold 교차검증 과정
+print("10-Folds Cross Validation:\n")
+for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
+    scores = cross_val_score(estimator=clf,
+                               X=x_train,
+                               y=y_train,
+                               cv=10,
+                               scoring='roc_auc')
+    print("ROC_AUC : %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+print("\n")
+
+### 다수결 투표 분류기 생성 및 학습
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
+clf_labels += ['Majority Voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf,
+                            X=x_train,
+                            y=y_train,
+                            cv=10,
+                            scoring='roc_auc')
+    print("ROC_AUC : %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+### 학습 결과에 대한 성능평가 (ROC Curve)
+colors = ["black", "orange", "blue", "green"]
+linestyles = [":", "--", "-.", "-"]
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    y_pred = clf.fit(x_train, y_train).predict_proba(x_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+
+    plt.plot(fpr, tpr, color=clr, linestyle=ls, label='%s (auc = %0.2f)' % (label, roc_auc))
+    plt.legend(loc="lower right")
+    plt.plot([0, 1], [0, 1], linestyle="--", color="gray", linewidth=2)
+    plt.xlim([-0.1, 1.1])
+    plt.ylim([-0.1, 1.1])
+    plt.grid(alpha=0.5)
+    plt.xlabel("False Positive Rate(FPR)")
+    plt.ylabel("True Positive Rate(FPR)")
+    plt.show()
+
+# Bagging
