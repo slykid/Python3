3a9fdea14d013e0f14b7326253875a8bf5a00db1
Kilhyun Kim <kid1064@gmail.com>
2019-11-24 00:37:45 +0900

    Python Update Package : Pillow-6.2.1

--- classification.py
+++ classification.py
@@ -725,4 +725,75 @@ for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
     plt.ylabel("True Positive Rate(FPR)")
     plt.show()
 
-# Bagging
+# Boosting
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header=None)
+dataset.columns = ["Class label", "Alcohol", "Malic acid", "Ash", "Alcalinity of ash",
+                   "Magnesium", "Total phenols", "Flavanoids", "Nonflavanoids phenols",
+                   "Proanthocyanins", " Color intensity", "Hue",
+                   "OD280/OD315 of diluted wines", "Proline"]
+
+dataset = dataset[dataset["Class label"] != 1]
+y = dataset["Class label"].values
+x = dataset[["Alcohol", "OD280/OD315 of diluted wines"]].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1, stratify=y)
+
+tree = DecisionTreeClassifier(criterion="entropy", random_state=1, max_depth=1)
+ada = AdaBoostClassifier(base_estimator=tree, n_estimators=500, learning_rate=0.1, random_state=1)
+
+tree = tree.fit(x_train, y_train)
+y_train_pred = tree.predict(x_train)
+y_test_pred = tree.predict(x_test)
+
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print("의사결정나무의 훈련정확도 / 테스트정확도 : %.3f / %.3f" % (tree_train, tree_test))
+
+ada = ada.fit(x_train, y_train)
+y_train_pred = ada.predict(x_train)
+y_test_pred = ada.predict(x_test)
+
+ada_train = accuracy_score(y_train, y_train_pred)
+ada_test = accuracy_score(y_test, y_test_pred)
+print("AdaBoosting의 훈련정확도 / 테스트정확도 : %.3f / %.3f" % (ada_train, ada_test))
+
+x_min = x_train[:, 0].min() - 1
+x_max = x_train[:, 0].max() + 1
+y_min = x_train[:, 1].min() - 1
+y_max = x_train[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(1, 2, sharex="col", sharey="row", figsize=(8, 3))
+
+for idx, clf, tt in zip([0, 1], [tree, ada], ["Decision Tree", "AdaBoost"]):
+    clf.fit(x_train, y_train)
+    z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    z = z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, z, alpha=0.3)
+    axarr[idx].scatter(x_train[y_train==0, 0],
+                       x_train[y_train==0, 1],
+                       c="blue",
+                       marker="^")
+    axarr[idx].scatter(x_train[y_train==1, 0],
+                       x_train[y_train==1, 1],
+                       c="red",
+                       marker="o")
+    axarr[idx].set_title(tt)
+    axarr[0].set_ylabel("Alcohol", fontsize=12)
+plt.text(10.2, -0.5, s="OD280/OD315 of diluted wines", ha="center", va="center", fontsize=12)
+plt.tight_layout()
+plt.show()
