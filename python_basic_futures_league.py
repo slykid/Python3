0ea6c0e399ee911f8fd1b41f433db703f8668ff1
kilhyunkim <slykid@naver.com>
2020-08-18 17:29:28 +0900

    BigStudy : Futures League example - Data Exploring

---
+++ python_basic_futures_league.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import pandas as pd
+
+# Q1. 2020년 데이터만 로드하시오.
+# 팀 데이터는 파일인코딩을 'CP949' 로 설정 후 로드한다.
+## 개인타자(df_hitter), 개인투수 (df_pitcher),  경기(df_game), 등록선수(df_entry), 선수(df_player),
+## 팀(df_team), 팀타자(df_team_hitter), 팀투수(df_team_pitcher)
+df_hitter =
+df_pitcher =
+df_game =
+df_entry =
+df_player =
+df_team =
+df_team_hitter =
+df_team_pitcher =
+
+# Q2. 개인타자 중 SK 소속인 타자들의 ID(P_ID)를 df_hitter_sk 에 저장하시오.
+df_hitter_sk =
+
+# Q3. 개인타자 중 타율(HIT)이 3.0 이상인 타자들만 df_hitter_3_0 에 저장하시오.
+df_hitter_3_0 =
+
+
