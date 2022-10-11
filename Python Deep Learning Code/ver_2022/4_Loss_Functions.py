3ec1f6e82d3a1e0a4bc04d48ba53b8b98fd7b048
slykid <slykid@naver.com>
2022-10-11 22:22:55 +0900

    [DeepLearning] 4. Loss Function

--- Python Deep Learning Code/ver_2022/4_Loss_Functions.py
+++ Python Deep Learning Code/ver_2022/4_Loss_Functions.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tensorflow as tf
 
 # 1. Make Dataset
@@ -65,3 +64,27 @@ print("X(shape/dtype/data): {} / {}\n{}\n".format(X.shape, X.dtype, X.numpy()))
 print("Y(shape/dtype/data): {} / {}\n{}\n".format(Y.shape, Y.dtype, Y.numpy()))
 
 # 1-4. Dataset Object
+import tensorflow as tf
+
+N, n_features = 100, 5
+batch_size = 32
+
+t_weights = tf.constant([1, 2, 3, 4, 5], dtype=tf.float32)
+t_bias = tf.constant([10], dtype=tf.float32)
+
+X = tf.random.normal(mean=0, stddev=1, shape=(N, n_features))
+Y = tf.reduce_sum(t_weights * X, axis=1) + t_bias
+
+# for batch_idx in range(N // batch_size):
+#     x = X[batch_idx * batch_size : (batch_idx + 1) * batch_size]
+#     y = Y[batch_idx * batch_size: (batch_idx + 1) * batch_size]
+# 위의 과정대로 생성할 수 있지만 매우 복잡함
+
+# 이를 위해 아래와 같이 텐서플로에서 제공하는 함수를 사용하면 쉬움
+# 또한 데이터를 어떻게 뽑아서 모델에 주는 지에 대한 것도 중요함!
+dataset = tf.data.Dataset.from_tensor_slices((X, Y))  # from_tensor_slices 는 사용자가 임의로 텐서를 생성하는 경우에만 사용함
+dataset = dataset.batch(batch_size)
+
+for x, y in dataset:
+    print("X: {} \n\nY: {}".format(x, y))
+    print(x.shape, y.shape)
