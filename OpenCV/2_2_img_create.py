01cd6a8773366d11f989d1822c95a07c01274d84
slykid <slykid@naver.com>
2022-10-13 17:53:29 +0900

    [OpenCV] 2. OpenCV 기초

--- OpenCV/2_2_img_create.py
+++ OpenCV/2_2_img_create.py
@@ -39,3 +39,21 @@ cv2.waitKey()
 cv2.destroyAllWindows()
 
 # 3. 부분 영상 추출
+img1 = cv2.imread('data/opencv/ch02/HappyFish.jpg')
+
+img2 = img1[40:120, 30:150]
+img3 = img1[40:120, 30:150].copy()
+
+cv2.circle(img2, (50, 50), 20, (0, 0, 225), 2)
+
+cv2.imshow('img1', img1)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+cv2.imshow('img2', img2)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+cv2.imshow('img3', img3)
+cv2.waitKey()
+cv2.destroyAllWindows()
