41b33d39f5625d33f395329034a4ebd0e4760e9e
slykid <slykid@naver.com>
2023-07-22 17:31:34 +0900

    [Pytorch] 5. Pytorch Dataset

--- Pytorch/basic/5_pytorch_dataset.py
+++ Pytorch/basic/5_pytorch_dataset.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
 cancer = load_breast_cancer()
 
@@ -22,8 +23,8 @@ data = torch.from_numpy(df.values).float()
 data.shape  # torch.Size([569, 31])
 
 x = data[:, :10]
-y = data[:, :-1]
-print("x.shape: " + x.shape ", y.shape: " + y.shape)
+y = data[:, -1:]
+print("x.shape: " + str(x.shape) + ", y.shape: " + str(y.shape))
 
 ratio = [.6, .2, .2]  # train, valid, test 순
 
@@ -43,4 +44,44 @@ y = y.split(cnts, dim=0)
 
 for x_i, y_i in zip(x, y):
     print(x_i.size(), y_i.size())
+
+# Set Hyperparmeter
+n_epochs = 10000
+batch_size = 128
+print_interval = 500
+early_stop = 100
+
+class CustomDataset(Dataset):
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+
+        super().__init__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
+
+train_loader = DataLoader(
+    dataset=CustomDataset(x[0], y[0]),
+    batch_size=batch_size,
+    shuffle=True
+)
+
+valid_loader = DataLoader(
+    dataset=CustomDataset(x[1], y[1]),
+    batch_size=batch_size,
+    shuffle=False
+)
+
+test_loader = DataLoader(
+    dataset=CustomDataset(x[2], y[2]),
+    batch_size=batch_size,
+    shuffle=False
+)
+
+print("Train %d / Valid %d / Test %d" % (len(train_loader.dataset), len(valid_loader.dataset), len(test_loader.dataset)))
+
 
