282384ffddffb06ae0a07bb9c899728afb90ef7a
slykid <slykid@naver.com>
2023-08-02 23:04:51 +0900

    [Pytorch] POS Menu 분류

--- Field Test Code/pytorch_tokenizer.py
+++ Field Test Code/pytorch_tokenizer.py
@@ -17,52 +17,7 @@ import torch.optim as optim
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
 
-from tokenizers import BertWordPieceTokenizer
-from transformers import BertTokenizerFast, BertModel, TFBertModel
-
-# 변수 설정
-num_class = 806  # pos menu3_nm 기준
-
-os.environ["TOKENIZERS_PARALLELISM"] = 'true'
-
-# device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
-print(f'사용 디바이스: {device}')
-
-# 데이터 로드
-# data = pd.read_csv("data/pos_menu/menu_data_2022.csv", sep=",", encoding="utf-8")
-data = pd.read_csv("data/pos_menu/menu_data_jeonju.csv", sep=",", encoding="utf-8")
-data
-
-# 학습용 테스트용 분리
-train = data.loc[-data.isna().any(axis=1)]
-test = data.loc[data.isna().any(axis=1)]
-
-# 문자 길이 컬럼 추가
-train["length"] = train["prod_nm"].apply(lambda x: len(str(x)))
-test["length"] = test["prod_nm"].apply(lambda x: len(str(x)))
-
-label = pd.unique(train.menu3_nm.apply(lambda x: str(x))).tolist()
-label.sort()
-label = [x for x in label if x != 'nan']
-label_num = {word : num for num, word in enumerate(label)}
-train["label"] = train["menu3_nm"].map(label_num)
-
-# tokenizer
-tokenizer= BertTokenizerFast.from_pretrained('kykim/bert-kor-base',strip_accents=False, lowercase=False)  # 로드
-tokenizer.get_vocab()
-
-# tokenizer에 special token 추가
-user_defined_symbols = ['[BOS]','[EOS]','[UNK0]','[UNK1]','[UNK2]','[UNK3]','[UNK4]','[UNK5]','[UNK6]','[UNK7]','[UNK8]','[UNK9]']
-unused_token_num = 200
-unused_list = ['[unused{}]'.format(n) for n in range(unused_token_num)]
-user_defined_symbols = user_defined_symbols + unused_list
-
-tokenizer.all_special_tokens
-special_tokens_dict = {'additional_special_tokens': user_defined_symbols}
-tokenizer.add_special_tokens(special_tokens_dict)
-tokenizer.all_special_tokens
-
+from transformers import BertTokenizerFast, BertModel, BertConfig
 
 class TokenDataset(Dataset):
 
@@ -103,6 +58,63 @@ class TokenDataset(Dataset):
         }, torch.tensor(label)
 
 
+# 변수 설정
+num_class = 806  # pos menu3_nm 기준
+
+os.environ["TOKENIZERS_PARALLELISM"] = 'true'
+
+device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
+print(f'사용 디바이스: {device}')
+
+# 데이터 로드
+# data = pd.read_csv("data/pos_menu/menu_data_2022.csv", sep=",", encoding="utf-8")
+data = pd.read_csv("data/pos_menu/menu_data_jeonju.csv", sep=",", encoding="utf-8")
+data
+
+# 학습용 테스트용 분리
+train = data.loc[-data.isna().any(axis=1)]
+test = data.loc[data.isna().any(axis=1)]
+
+# 문자 길이 컬럼 추가
+train["label"] = 0
+label = pd.unique(train.menu3_nm.apply(lambda x: str(x))).tolist()
+label.sort()
+label = [x for x in label if x != 'nan']
+label_num = {word : num for num, word in enumerate(label)}
+train["label"] = train["menu3_nm"].apply(lambda x: label_num.get(x))
+
+train["length"], test["length"] = 0, 0
+train["length"] = train["prod_nm"].apply(lambda x: len(str(x)))
+test["length"] = test["prod_nm"].apply(lambda x: len(str(x)))
+
+train["id"], test["id"] = 0, 0
+train["id"] = [x for x in range(1, len(train.prod_nm) + 1)]
+test["id"] = [x for x in range(1, len(test.prod_nm) + 1)]
+
+train = train[["id", "prod_nm", "label", "length"]]
+test = test[["id", "prod_nm", "length"]]
+
+train = train.reset_index(drop=True)
+test = test.reset_index(drop=True)
+
+# tokenizer
+tokenizer = BertTokenizerFast.from_pretrained("kykim/bert-kor-base")
+tokenizer.get_vocab()
+
+# tokenizer에 special token 추가
+user_defined_symbols = ['[BOS]','[EOS]','[UNK0]','[UNK1]','[UNK2]','[UNK3]','[UNK4]','[UNK5]','[UNK6]','[UNK7]','[UNK8]','[UNK9]']
+unused_token_num = 200
+unused_list = ['[unused{}]'.format(n) for n in range(unused_token_num)]
+user_defined_symbols = user_defined_symbols + unused_list
+
+tokenizer.all_special_tokens
+special_tokens_dict = {'additional_special_tokens': user_defined_symbols}
+tokenizer.add_special_tokens(special_tokens_dict)
+tokenizer.all_special_tokens
+
+
+
 CHECKPOINT_NAME = 'kykim/bert-kor-base'
 tokenizer_pretrained = CHECKPOINT_NAME
 
@@ -116,4 +128,28 @@ test_loader = DataLoader(test_data, batch_size=2, shuffle=True, num_workers=2)
 
 inputs, labels = next(iter(train_loader))
 
+# 데이터셋을 device 설정
+inputs = {k: v.to(device) for k, v in inputs.items()}
+labels.to(device)
 
+# 생성된 inputs의 key 값 출력
+inputs.keys()
+
+# key 별 shape 확인
+inputs['input_ids'].shape, inputs['attention_mask'].shape, inputs['token_type_ids'].shape
+
+config = BertConfig.from_pretrained(CHECKPOINT_NAME)
+config
+
+# labels 출력
+labels
+
+# 모델 생성
+model_bert = BertModel.from_pretrained(CHECKPOINT_NAME).to(device)
+model_bert
+
+# loss 정의: CrossEntropyLoss
+loss_fn = nn.CrossEntropyLoss()
+
+# 옵티마이저 정의: bert.paramters()와 learning_rate 설정
+optimizer = optim.Adam(bert.parameters(), lr=1e-5)
