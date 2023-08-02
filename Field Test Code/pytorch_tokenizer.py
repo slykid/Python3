7c21b15f7f41af409992fa1ec129ca2590c39b34
slykid <slykid@naver.com>
2023-08-02 17:45:07 +0900

    [Pytorch] POS MENU Modeling

--- Field Test Code/pytorch_tokenizer.py
+++ Field Test Code/pytorch_tokenizer.py
@@ -20,6 +20,9 @@ from torch.utils.data import Dataset, DataLoader
 from tokenizers import BertWordPieceTokenizer
 from transformers import BertTokenizerFast, BertModel, TFBertModel
 
+# 변수 설정
+num_class = 806  # pos menu3_nm 기준
+
 os.environ["TOKENIZERS_PARALLELISM"] = 'true'
 
 # device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
@@ -27,11 +30,8 @@ device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
 print(f'사용 디바이스: {device}')
 
 # 데이터 로드
-# data = pd.read_csv("D:/workspace/Python3/data/pos_menu/menu_data_2022.csv", sep=",", encoding="utf-8") # ver. Windows
-# data = pd.read_csv("~/workspace/Python3/data/pos_menu/menu_data_2022.csv", sep=",", encoding="utf-8") # ver. mac, linux
-
-# data = pd.read_csv("D:/workspace/Python3/data/pos_menu/menu_data_jeonju.csv", sep=",", encoding="utf-8")
-data = pd.read_csv("~/workspace/Python3/data/pos_menu/menu_data_jeonju.csv", sep=",", encoding="utf-8")
+# data = pd.read_csv("data/pos_menu/menu_data_2022.csv", sep=",", encoding="utf-8")
+data = pd.read_csv("data/pos_menu/menu_data_jeonju.csv", sep=",", encoding="utf-8")
 data
 
 # 학습용 테스트용 분리
@@ -42,49 +42,28 @@ test = data.loc[data.isna().any(axis=1)]
 train["length"] = train["prod_nm"].apply(lambda x: len(str(x)))
 test["length"] = test["prod_nm"].apply(lambda x: len(str(x)))
 
+label = pd.unique(train.menu3_nm.apply(lambda x: str(x))).tolist()
+label.sort()
+label = [x for x in label if x != 'nan']
+label_num = {word : num for num, word in enumerate(label)}
+train.menu3_nm = train.menu3_nm.map(label_num)
+
+
+
 # tokenizer
-tokenizer = BertWordPieceTokenizer(strip_accents=False, lowercase=False)
-tokenizer.train(
-    # files=["D:/workspace/Python3/data/pos_menu/menu_data_2022.csv"],
-    files=["/Users/slykid/workspace/Python3/data/pos_menu/menu_data_2022.csv"],
-    vocab_size=32000,
-    min_frequency=5,
-    limit_alphabet=6000,
-    show_progress=True
-)
-print('train complete')
+# tokenizer = BertWordPieceTokenizer(strip_accents=False, lowercase=False)
+tokenizer= BertTokenizerFast.from_pretrained('kykim/bert-kor-base',strip_accents=False, lowercase=False)  # 로드
+#
+# tokenizer.train(
+#     files=["data/pos_menu/menu_data_2022.csv"],
+#     vocab_size=32000,
+#     min_frequency=5,
+#     limit_alphabet=6000,
+#     show_progress=True
+# )
+# print('train complete')
 
-user_defined_symbols = ['[BOS]','[EOS]','[UNK0]','[UNK1]','[UNK2]','[UNK3]','[UNK4]','[UNK5]','[UNK6]','[UNK7]','[UNK8]','[UNK9]']
-unused_token_num = 200
-unused_list = ['[unused{}]'.format(n) for n in range(unused_token_num)]
-user_defined_symbols = user_defined_symbols + unused_list
-
-special_tokens_dict = {'additional_special_tokens': user_defined_symbols}
-
-sentence = "광어한판 10pcs"
-output = tokenizer.encode(sentence)
-
-print(sentence)
-print('=>idx   : %s'%output.ids)
-print('=>tokens: %s'%output.tokens)
-print('=>offset: %s'%output.offsets)
-print('=>decode: %s\n'%tokenizer.decode(output.ids))
-
-hf_model_path='result/pos_menu/tokenizer_model'
-if not os.path.isdir(hf_model_path):
-    os.mkdir(hf_model_path)
-tokenizer.save_model(hf_model_path)
-
-# Tokenizer 테스트
-tokenizer_for_load = BertTokenizerFast.from_pretrained(hf_model_path,
-                                                       strip_accents=False,  # Must be False if cased model
-                                                       lowercase=False)  # 로드
-
-# vocab check
-tokenizer_for_load.get_vocab()
-
-# special token check
-tokenizer_for_load.all_special_tokens # 추가하기 전 기본적인 special token
+tokenizer.get_vocab()
 
 # tokenizer에 special token 추가
 user_defined_symbols = ['[BOS]','[EOS]','[UNK0]','[UNK1]','[UNK2]','[UNK3]','[UNK4]','[UNK5]','[UNK6]','[UNK7]','[UNK8]','[UNK9]']
@@ -92,12 +71,52 @@ unused_token_num = 200
 unused_list = ['[unused{}]'.format(n) for n in range(unused_token_num)]
 user_defined_symbols = user_defined_symbols + unused_list
 
+tokenizer.all_special_tokens
 special_tokens_dict = {'additional_special_tokens': user_defined_symbols}
-tokenizer_for_load.add_special_tokens(special_tokens_dict)
+tokenizer.add_special_tokens(special_tokens_dict)
+tokenizer.all_special_tokens
 
-# check tokenizer vocab with special tokens
-print('check special tokens : %s'%tokenizer_for_load.all_special_tokens[:20])
 
-# save tokenizer model with special tokens
-tokenizer_for_load.save_pretrained(hf_model_path+'_special')
+class TokenDataset(Dataset):
+
+    def __init__(self, dataframe, tokenizer_pretrained):
+        # sentence, label 컬럼으로 구성된 데이터프레임 전달
+        self.data = dataframe
+        # Huggingface 토크나이저 생성
+        self.tokenizer = BertTokenizerFast.from_pretrained(tokenizer_pretrained)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        sentence = self.data.iloc[idx]['prod_nm']
+        label = self.data.iloc[idx]['label']
+
+        # 토큰화 처리
+        tokens = self.tokenizer(
+            sentence,  # 1개 문장
+            return_tensors='pt',  # 텐서로 반환
+            truncation=True,  # 잘라내기 적용
+            padding='max_length',  # 패딩 적용
+            add_special_tokens=True  # 스페셜 토큰 적용
+        )
+
+        input_ids = tokens['input_ids'].squeeze(0)  # 2D -> 1D
+        attention_mask = tokens['attention_mask'].squeeze(0)  # 2D -> 1D
+        token_type_ids = torch.zeros_like(attention_mask)
+
+        # input_ids, attention_mask, token_type_ids 이렇게 3가지 요소를 반환하도록 합니다.
+        # input_ids: 토큰
+        # attention_mask: 실제 단어가 존재하면 1, 패딩이면 0 (패딩은 0이 아닐 수 있습니다)
+        # token_type_ids: 문장을 구분하는 id. 단일 문장인 경우에는 전부 0
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'token_type_ids': token_type_ids,
+        }, torch.tensor(label)
+
+
+
+
+
 
