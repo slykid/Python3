4e0a550105485409bc250f79b3217f184c5185ff
slykid <slykid@naver.com>
2023-08-13 23:07:04 +0900

    [Pytorch] POS Menu 분류 업데이트('23.08.13)

--- Field Test Code/pos_mkdata.py
+++ Field Test Code/pos_mkdata.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from kiwipiepy import Kiwi
-from gensim.models import word2vec
+from gensim.models import Word2Vec
+
+from matplotlib import pyplot as plt
 
 
 def noun_extract(text):
@@ -19,15 +21,35 @@ data = pd.read_csv("data/pos_menu/pos_menu.csv")
 
 kiwi = Kiwi()
 data["keywords"] = data["prod_nm"].apply(noun_extract)
+keywords = sum(data["keywords"], [])
+
 data["keywords"] = data["keywords"].apply(lambda x: " ".join(x))
 data = data[["id", "prod_nm", "keywords", "label", "length"]]
+df_keyword = pd.DataFrame(keywords, columns=["keyword"])
+
 data.to_csv("data/pos_menu/pos_menu_kiwi.csv", index=False)
+df_keyword.to_csv("data/pos_menu/pos_keyword.csv", index=False)
 
 # 불용어 처리를 위한 단어간 군집화
 data = pd.read_csv("data/pos_menu/pos_menu_kiwi.csv")
+keyword = pd.read_csv("data/pos_menu/pos_keyword.csv")
 
 ## 토크나이징 결과 취합
-tokens = list(set(sum([x.split(" ") for x in data["keywords"].tolist()], [])))
 tokens = tokens.sort()
 
+## 단어별 분포 확인
+plt.plot()
+
+
 ## skip gram 을 통한 임베딩
+model = Word2Vec(sentences=tokens, window=3, min_count=1, sg=1, iter=1000)
+model.init_sims(replace=True)
+
+vectors = model.wv
+vocabs = vectors.vocab.keys()
+vector_list = [vectors[v] for v in vocabs]
+
+## 테스트
+print(vectors.similarity("연어", "생선"))
+print(vectors.similarity("연어", "알람"))
+
