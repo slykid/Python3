ccd09f2b89a0bf7c99700fc91a0dad93027323d7
slykid <slykid@naver.com>
2024-04-25 20:23:11 +0900

    [Data Analysis] 9. 가설검정

---
+++ DataAnalysis_Blog/9_hypothesis_testing.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.stats import norm
+
+sample = np.array([152.8, 148.9, 149.6, 158.2, 162.9, 139.6, 153.5, 161.3])
+
+z = (np.mean(sample) - 152) / (10 / np.sqrt(8))
+print(z)
+
+p_value = 1 - norm.cdf(z)
+print(p_value)
