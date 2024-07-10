b89db1a04c26e3353e3728775e0d5298323165e5
slykid <slykid@naver.com>
2024-07-10 22:15:54 +0900

    FEAT: Update '24.07.10 * 용도별 하위 프로젝트로 재편

--- DataAnalysis/AnomalyDetection/Exercise/water_circulation_system/EDA.py
+++ DataAnalysis/AnomalyDetection/Exercise/water_circulation_system/EDA.py
@@ -7,3 +7,8 @@ from matplotlib import pyplot as plt
 import pyod
 
 matplotlib.use("MacOSX")
+pd.set_option("display.max_columns", 100)
+pd.set_option("display.max_rows", 100)
+
+df = pd.read_csv("Dataset/anomaly_detection/water_circulation_system/chapter01_df.csv", sep=";")
+df.head()
