235a5acf8423d2e02419c23d208204fb000e496f
slykid <slykid@naver.com>
2024-09-04 21:07:48 +0900

    FEAT: [AnomalyDetection] 2. Bearing Failure Anomaly Detection * 베어링 마모에 따른 교체주기 예측 데이터 EDA

--- DataAnalysis/AnomalyDetection/Exercise/bearing_failure/EDA.py
+++ DataAnalysis/AnomalyDetection/Exercise/bearing_failure/EDA.py
@@ -6,6 +6,13 @@ import os
 import numpy as np
 import pandas as pd
 
+import matplotlib
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+matplotlib.use("MacOSX")
+plt.style.use(['dark_background'])
+
 # _path = "/Volumes/LaCie/workspace/Python/Dataset/anomaly_detection/bearing_failure"
 _path = "/Users/kilhyunkim/workspace/Python3/DataAnalysis/Dataset/anomaly_detection/bearing_failure/"
 
@@ -32,7 +39,14 @@ if os.path.exists(_path) is False:
     data.to_csv(_path + "/data.csv", index=True)
 else:
     print("File exists!!")
-    data = pd.read_csv(_path + "data.csv", index_col="Unnamed: 0")
+    data = pd.read_csv(_path + "data.csv")
+
+    # time 컬럼에 대한 추가적인 인덱스 변환 방법
+    data["Unnamed: 0"] = pd.to_datetime(data["Unnamed: 0"])
+    data.set_index("Unnamed: 0", inplace=True)
+    data.index.name = ''
+
+data.head()
 
 # 2. 문제해결 프로세스 정의
 # 2.1 문제정의
@@ -53,6 +67,7 @@ else:
 # - 모델 활용 전/후의 Line stop 횟수 비교
 # - 모델 활용 전/후 계획 대비 생산률 비교
 
+
 # 3. 데이터 전처리 및 EDA
 # 3.1 데이터 형태 확인
 print("data shape:", data.shape)
@@ -61,3 +76,56 @@ print(data.info())
 # 3.2 NULL 값 확인
 print(data.isnull().sum())
 
+# 3.3 기술통계 확인
+print(data.describe())
+
+# 3.4 EDA
+data.head()
+print("Min datetime:", data.index.min())
+print("Max datetime:", data.index.max())
+
+plt.figure(figsize=(24, 5))
+plt.plot(data.index, data["Bearing1"], linestyle='-', color='red', label='1')
+plt.plot(data.index, data["Bearing2"], linestyle='-', color='yellow', label='2')
+plt.plot(data.index, data["Bearing3"], linestyle='-', color='green', label='3')
+plt.plot(data.index, data["Bearing4"], linestyle='-', color='lightblue', label='4')
+plt.legend()
+
+# 2004.02.17 일 이전 시점 데이터 확인
+before = data[:"2004-02-17 00:00:00"]
+plt.figure(figsize=(24, 5))
+plt.plot(before.index, before["Bearing1"], linestyle='-', color='red', label='1')
+plt.plot(before.index, before["Bearing2"], linestyle='-', color='yellow', label='2')
+plt.plot(before.index, before["Bearing3"], linestyle='-', color='green', label='3')
+plt.plot(before.index, before["Bearing4"], linestyle='-', color='lightblue', label='4')
+plt.legend()
+
+# 2004.02.17 일 이후 시점 데이터 확인
+after = data["2004-02-17 00:00:00":]
+plt.figure(figsize=(24, 5))
+plt.plot(after.index, after["Bearing1"], linestyle='-', color='red', label='1')
+plt.plot(after.index, after["Bearing2"], linestyle='-', color='yellow', label='2')
+plt.plot(after.index, after["Bearing3"], linestyle='-', color='green', label='3')
+plt.plot(after.index, after["Bearing4"], linestyle='-', color='lightblue', label='4')
+plt.legend()
+
+sns.displot(data["Bearing1"])
+
+# 4. 시계열 데이터 특성 추출
+data["year"] = data.index.year
+data["month"] = data.index.month
+data["day"] = data.index.weekday
+data["hour"] = data.index.hour
+data["date"] = data.index.date
+
+data.head()
+
+# 4.1 Bearing 일별 분포 확인
+n_col = 1
+n_row = 4
+
+fig, ax = plt.subplots(ncols=n_col, nrows=n_row, figsize=(20, n_row * 5))
+for row, col in enumerate(data.columns[0:4]):
+    sns.boxplot(x='date', y=col, data=data, ax=ax[int(row%n_row)])
+
+# plt.savefig()
